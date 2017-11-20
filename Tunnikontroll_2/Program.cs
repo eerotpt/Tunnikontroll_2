13b2b6a1932e869132e9e133d2cf60a3f007527a
eerotpt <eero.sakson@tptlive.ee>
2017-11-20 13:36:59 +0200

    Initial commit

---
+++ Tunnikontroll_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnikontroll_2
+{
+    class Program
+    {
+        //double number_1;
+        static void Main(string[] args)
+        {
+            double total = 0;
+            double value = 0;
+            double number = 0;
+
+            do
+            {
+                Console.Write("Enter value: ");
+                string number_1 = Console.ReadLine();
+                Console.WriteLine();
+                
+                number = double.Parse(number_1);
+                total += number;
+
+            } while (number != -1);
+
+            total += 1;
+
+            Console.WriteLine("total = " + total);
+            Console.WriteLine("15 % Gratuity: €" + total * 0.15);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
